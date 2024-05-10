f5036459b83dd22c6ed7076791f0873e92860a7a
Mahdi Ahmadi Kamal <909stars@gmail.com>
2024-05-10 20:48:11 +0430

    3rd commit

--- PyLearn7-Assignment37/numbers dataset/numbers_dateset.py
+++ PyLearn7-Assignment37/numbers dataset/numbers_dateset.py
@@ -5,12 +5,13 @@ img = cv2.imread("input/image.png")
 rows, cols, _ = img.shape
 
 num=0
-i=1
+count=0
 
 for row in range(0, rows, 20):
     for col in range(0, cols, 20):
         
-        
-        
-    row += 20
-     
+        crop_img = img[row:row+20, col:col+20]
+        cv2.imwrite(f"output/{num}/{count}.jpg", crop_img)
+        count += 1
+        if count%500 == 0:
+            num += 1
