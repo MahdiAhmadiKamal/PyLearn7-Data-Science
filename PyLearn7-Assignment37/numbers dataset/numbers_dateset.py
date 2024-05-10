4353958edeef1a19de3ef9b9325ca3513e4f48ba
Mahdi Ahmadi Kamal <909stars@gmail.com>
2024-05-10 20:27:29 +0430

    2nd commit

---
+++ PyLearn7-Assignment37/numbers dataset/numbers_dateset.py
@@ -0,0 +1,16 @@
+import cv2
+
+
+img = cv2.imread("input/image.png")
+rows, cols, _ = img.shape
+
+num=0
+i=1
+
+for row in range(0, rows, 20):
+    for col in range(0, cols, 20):
+        
+        
+        
+    row += 20
+     
